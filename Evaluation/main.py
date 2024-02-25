33e546be60e9bd4f0e7e59ec23c033356b21a676
Andrew Huard <ahuard@ucsb.edu>
2024-02-24 16:23:18 -0800

    Evaluation Plot With Fake Data

--- Evaluation/main.py
+++ Evaluation/main.py
@@ -1,16 +1,56 @@
-# This is a sample Python script.
+import numpy as np
+import matplotlib.pyplot as plt
+import pandas as pd
 
-# Press ⌃R to execute it or replace it with your code.
-# Press Double ⇧ to search everywhere for classes, files, tool windows, actions, and settings.
 
+def start():
+    # Load data from Excel spreadsheet
+    df = pd.read_excel('Evaluation_Data.xlsx')
 
-def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press ⌘F8 to toggle the breakpoint.
+    # Get unique azimuth and range values
+    azimuths = np.deg2rad(df['Azimuth']).unique()
+    ranges = df['Range'].unique()
+    error = df['Error']
+
+    # Create meshgrid for r and theta
+    theta, r = np.meshgrid(azimuths, ranges)
+
+    # Reshape error vector to match the shape of r and theta meshgrid
+    error_reshaped = error.values.reshape(theta.shape)
+
+    # Create a polar plot
+    fig, ax = plt.subplots(subplot_kw={'projection': 'polar'})
+
+    # Plot the data with colormap 'magma'
+    c = ax.pcolormesh(theta, r, error_reshaped, cmap='magma', shading='auto', vmin=0, vmax=10)
+
+    # Add a colorbar
+    plt.colorbar(c, ax=ax, label='Intensity')
+
+    # Set azimuth ticks every 5 degrees
+    ax.set_xticks(np.deg2rad(np.arange(-35, 36, 5)))
+
+    # Hide the tick lines but keep the tick marks visible
+    ax.tick_params(axis='both', which='both', length=5, width=0)
+
+    # Set labels
+    ax.set_xlabel('Azimuth (degrees)')
+    ax.set_ylabel('Range (meters)')
+
+    # Adjust the plot to have a fan shape
+    ax.set_thetamin(-35)
+    ax.set_thetamax(35)
+
+    # Set radial limit to 1 meter
+    ax.set_ylim(0, 5)  # Adjust the range limit according to your data range
+
+    # Hide radial axes beyond r < 1
+    ax.spines['polar'].set_visible(False)
+
+    # Show the plot
+    plt.show()
 
 
 # Press the green button in the gutter to run the script.
 if __name__ == '__main__':
-    print_hi('PyCharm')
-
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
+    start()
